b892653cbbdd581ce3bd2e68220f3d692278a1a6
catbeat <1664158108@qq.com>
2019-10-27 04:23:35 -0400

    all_notes

---
+++ shell_notes.sh
@@ -0,0 +1,26 @@
+#notes for shell
+<<!
+to print sth
+echo "..."
+
+
+Variable
+
+1. Initialization
+
+a=123
+b=catbeat
+
+this is how to initialize the variable
+!!ps: do not have space between a=123 otherwise there would be error
+
+2. Print variable
+
+echo "\$a is $a"
+
+$variable will allow you to do this
+
+to just print $, you should add \$.
+
+
+!
